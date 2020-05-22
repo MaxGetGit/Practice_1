cbc170458b65d5868de714560a9d05885600a12b
Max <max.sv@mail.ru>
2020-05-22 13:10:08 +0300

    Циклы

--- js/original.js
+++ js/original.js
@@ -52,19 +52,40 @@
 //     console.log('Error');
 // }
 
-const num = 51;
+// const num = 51;
 
-switch (num) {
-    case 49:
-        console.log("Неверно");
-        break;
-    case 100:
-        console.log("Неверно");
-        break;
-    case 50:
-        console.log("Правда");
-        break;
-    default:
-        console.log("Не в этот раз!");
-        break;
+// switch (num) {
+//     case 49:
+//         console.log("Неверно");
+//         break;
+//     case 100:
+//         console.log("Неверно");
+//         break;
+//     case 50:
+//         console.log("Правда");
+//         break;
+//     default:
+//         console.log("Не в этот раз!");
+//         break;
+// }
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }
