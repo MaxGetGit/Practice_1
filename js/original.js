4edb361f824bf7379f74526d18e67d4ba409c370
Max <max.sv@mail.ru>
2020-05-22 17:22:36 +0300

    Методы

--- js/original.js
+++ js/original.js
@@ -177,3 +177,30 @@
 
 // const calc1 = (a, b) => { return a + b };
 // console.log(calc);
+
+// const str = 'test';
+
+// console.log(str.toUpperCase());
+// console.log(str);
+
+// let fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('fruit'));
+
+// const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
