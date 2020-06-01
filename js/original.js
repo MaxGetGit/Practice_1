d73338853e3286ed057cadd6ec19c14837a63a85
Max <max.sv@mail.ru>
2020-06-01 17:29:42 +0300

    Основы ООП

--- js/original.js
+++ js/original.js
@@ -388,18 +388,18 @@
 // products.sort();
 // console.log(products.join('; '));
 
-let a = 5,
-    b = a;
+// let a = 5,
+//     b = a;
 
-b = b + 5;
+// b = b + 5;
 
-console.log(b);
-console.log(a);
+// console.log(b);
+// console.log(a);
 
-const obj = {
-    a: 5,
-    b: 1
-};
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
 // const copy = obj; //Ссылка на уже существующий объект
 
@@ -408,78 +408,110 @@ const obj = {
 // console.log(copy);
 // console.log(obj);
 
-function copy (mainObj) {
-    let objCopy = {};
+// function copy (mainObj) {
+//     let objCopy = {};
 
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
 
-    return objCopy;
-}
+//     return objCopy;
+// }
 
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-};
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
 
-const newNumbers = copy(numbers);
+// const newNumbers = copy(numbers);
 
-newNumbers.a = 10;
+// newNumbers.a = 10;
 
 // console.log(newNumbers);
 // console.log(numbers);
 
-const add = {
-    d: 17,
-    e: 20
-};
+// const add = {
+//     d: 17,
+//     e: 20
+// };
 
-const clone = (Object.assign({}, add));
+// const clone = (Object.assign({}, add));
 
-clone.d = 20;
+// clone.d = 20;
 
 // console.log(add);
 // console.log(clone);
 
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
 
-newArray[1] = 'asdasdfs';
-console.log(oldArray);
-console.log(newArray);
+// newArray[1] = 'asdasdfs';
+// console.log(oldArray);
+// console.log(newArray);
 
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
 
-console.log(internet);
+// console.log(internet);
 
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
 
-const num = [2, 5, 7];
+// const num = [2, 5, 7];
 
-log(...num);
+// log(...num);
 
-const array = ['a', 'b'];
+// const array = ['a', 'b'];
 
-const Aarray = [...array];
+// const Aarray = [...array];
 
-console.log(Array);
+// console.log(Array);
 
-const q = {
-    one: 1,
-    two: 2
-};
+// const q = {
+//     one: 1,
+//     two: 2
+// };
 
-const newObj = {...q};
-console.log(newObj);
+// const newObj = {...q};
+// console.log(newObj);
+
+// let str = 'some';
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1, 2, 3]);
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Say hello');
+//     }
+// };
+
+// const john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier;
+
+// Object.setPrototypeOf(john, soldier);
+
+// console.log(john.armor);
+
+
+
+// const john = Object.create(soldier);
+
+// john.sayHello();
