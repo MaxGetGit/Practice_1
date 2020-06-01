dd29707ab2b0f9217e029fdae9bcc2484a08a561
Max <max.sv@mail.ru>
2020-06-01 17:12:22 +0300

    Передача по ссылке

--- js/original.js
+++ js/original.js
@@ -354,13 +354,13 @@
 
 // console.log(counter);
 
-const arr = [13, 2, 23, 16, 8];
-arr.sort(compareNum);
-console.log(arr);
+// const arr = [13, 2, 23, 16, 8];
+// arr.sort(compareNum);
+// console.log(arr);
 
-function compareNum(a, b) {
-    return a - b;
-}
+// function compareNum(a, b) {
+//     return a - b;
+// }
 
 // arr[99] = 0;
 // console.log(arr.length);
@@ -387,3 +387,99 @@ function compareNum(a, b) {
 // const products = str.split(', ');
 // products.sort();
 // console.log(products.join('; '));
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = obj; //Ссылка на уже существующий объект
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy (mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = (Object.assign({}, add));
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'asdasdfs';
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const Aarray = [...array];
+
+console.log(Array);
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+console.log(newObj);
